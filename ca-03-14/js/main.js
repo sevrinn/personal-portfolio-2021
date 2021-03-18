b74ee62e611bc7068e3a41b0ccb714d9326c63d1
Sevrinn <sevrinnsevrinn@gmail.com>
2021-03-18 05:39:42 -0700

    Added quform and new favicon.

---
+++ ca-03-14/js/main.js
@@ -0,0 +1,52 @@
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+const items = document.querySelectorAll(".item");
+
+/* Toggle mobile menu */
+function toggleMenu() {
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+
+    //Adds the hamburger icon
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+  } else {
+    menu.classList.add("active");
+
+    //adds the close (x) icon
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+  }
+}
+
+/* Activate Submenu */
+function toggleItem() {
+  if (this.classList.contains("submenu-active")) {
+    this.classList.remove("submenu-active");
+  } else if (menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+    this.classList.add("submenu-active");
+  } else {
+    this.classList.add("submenu-active");
+  }
+}
+
+/* Close submenu from anywhere */
+function closeSubmenu(e) {
+  let isClickInside = menu.contains(e.target);
+
+  if (!isClickInside && menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+  }
+}
+/* Event Listener toggle mobile menu*/
+toggle.addEventListener("click", toggleMenu, false);
+
+/* Event Listener activate submenu */
+for (let item of items) {
+  if (item.querySelector(".submenu")) {
+    item.addEventListener("click", toggleItem, false);
+    item.addEventListener("keypress", toggleItem, false);
+  }
+}
+
+/* Event listener close submenu*/
+document.addEventListener("click", closeSubmenu, false);
