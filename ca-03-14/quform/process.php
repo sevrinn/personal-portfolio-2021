224d30c7dc1fa863378f8885bf7a1c2c2700de8e
Sevrinn <sevrinnsevrinn@gmail.com>
2021-03-31 20:17:16 -0700

    Changed email on quform to Christina's

--- ca-03-14/quform/process.php
+++ ca-03-14/quform/process.php
@@ -39,7 +39,7 @@ $config['email'] = true;
  *     'recipient2@example.com'
  * );
  */
-$config['recipients'] = 'sevwelker@gmail.com';
+$config['recipients'] = 'contact@christinas-aesthetics.com';
 
 /**
  * Set the "From" address of the emails. You should set this to the contact
